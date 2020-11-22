b99ebd11c7cf549fde75f8846f78ef7f67a3f147
Rolls47 <quentin.adadain@gmail.com>
2020-11-22 18:05:14 +0100

    feat(form): add PropertyType and view

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Form\LocationType;
+use App\Form\PropertyType;
 use App\Repository\PropertyAccountingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 /**
@@ -12,6 +17,27 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class PropertyController extends AbstractController
 {
+    /**
+     * @Route ("/new", name="new")
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function new(EntityManagerInterface $em, Request $request): Response
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($property);
+            $em->flush();
+            return $this->redirectToRoute('home_index');
+        }
+        return $this->render('property/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/{slug}", name="accounting")
      * @param Property $property
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use App\Entity\Property;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom :'
+            ])
+            ->add('comment', TextType::class, [
+                'label' => 'Commentaire :'
+            ])
+            ->add('propertyType', EntityType::class, [
+                'class' => \App\Entity\PropertyType::class,
+                'label' => 'Type de propriété :'
+            ])
+            ->add('location', EntityType::class, [
+                'class' => Location::class,
+                'label' => 'Localisation :'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+        ]);
+    }
+}
